aa598a0b3b7b42d2614b4608bae3421cfb1424cf
H82M19\Student <Student>
2025-01-08 08:36:41 +0700

    JF part 2, code challenge#3

--- tuan01/main.js
+++ tuan01/main.js
@@ -1,11 +1,37 @@
-function calcTip(bills) {
-    return bills.map((bill, index) => {
-        if (bill >= 50 && bill <= 300) {
-            return bill * 1.15
-        } else {
-            return bill * 1.2
-        }
-    })
+const info = {
+    data1: {
+        mark: {
+            weights: 78,
+            tall: 1.69
+        },
+        john: {
+            weights: 92,
+            tall: 1.95
+        },
+    },
+    data2: {
+        mark: {
+            weights: 95,
+            tall: 1.88
+        },
+        john: {
+            weights: 85,
+            tall: 1.76
+        },
+    },
 }
 
-console.log(calcTip([125, 555, 44]))
+function BMI(data) {
+    let mass = data.weights
+    let height = data.tall
+    let result = mass / (height * height)
+    return result.toFixed(1)
+}
+
+var markHigherBMI = BMI(info.data2.mark) > BMI(info.data2.john)
+
+if (markHigherBMI) {
+    console.log(`Mark's BMI (${BMI(info.data2.mark)}) is higher than John's (${BMI(info.data2.john)})!`)
+} else {
+    console.log(`John's BMI (${BMI(info.data2.john)}) is higher than Mark's (${BMI(info.data2.mark)})!`)
+}
