10d2e47e1bf80b1ed9c9949288445becc1256a99
H82M19\Student <Student>
2025-01-08 08:24:56 +0700

    JF part 1, code challenge#4

--- tuan01/main.js
+++ tuan01/main.js
@@ -1,26 +1,18 @@
-const dolphins = [97, 112, 101]
-const koalas = [109, 95, 123]
 
+function calculateTip(billValue) {
+    
+    const tip = (billValue >= 50 && billValue <= 300) ? billValue * 0.15 : billValue * 0.20;
 
-function handleAverageScores(scores) {
-    let avgScores = 0;
-    scores.forEach(element => {
-        avgScores += element
-    });
-    return (avgScores / scores.length);
+    const totalValue = billValue + tip;
+    
+    console.log(`The bill was ${billValue}, the tip was ${tip.toFixed(2)}, and the total value ${totalValue.toFixed(2)}`);
 }
 
-if (handleAverageScores(dolphins) < 100 && (handleAverageScores(koalas) < 100)) {
-    console.log("noteam win!!");
-} else {
-    if (handleAverageScores(dolphins) === handleAverageScores(koalas)) {
-        console.log("two team draw!!")
-    } else if (handleAverageScores(dolphins) > handleAverageScores(koalas)) {
-        console.log("dolphins win!!")
-    } else {
-        console.log("koalas win!!")
-    }
-}
+
+const billValues = [275, 40, 430];
+
+billValues.forEach(bill => calculateTip(bill));
+
 
 
 
