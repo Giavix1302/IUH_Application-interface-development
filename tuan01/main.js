cccfef7a39e5652ddeedfc8ecd8504170c8122be
H82M19\Student <Student>
2025-01-08 08:13:59 +0700

    JF part 1, code challenge#2

--- tuan01/main.js
+++ tuan01/main.js
@@ -6,11 +6,13 @@ var John_height = 1.95
 function BMI(mass, height) {
     return mass / (height * height)
 }
-
-console.log("Data 1")
 console.log("Mark's BMI", BMI(Mark_mass, Mark_height))
 console.log("John's BMI", BMI(John_mass, John_height))
+if(BMI(Mark_mass, Mark_height) > BMI(John_mass, John_height)) {
+    console.log(`Mark's BMI (${BMI(Mark_mass, Mark_height)}) is higher than John's (${BMI(John_mass, John_height)})!`)
+} else {
+    console.log(`John's BMI (${BMI(John_mass, John_height)}) is higher than Mark's (${BMI(Mark_mass, Mark_height)})!`)
+}
 
-console.log("Data 2")
-console.log("Mark's BMI", BMI(95, 1.88))
-console.log("John's BMI", BMI(85, 1.76))
+
+
